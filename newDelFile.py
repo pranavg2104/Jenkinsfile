80aa980e1a915d98eced5d3f68ca2b5571268cf5
pranavg2104 <80096043+pranavg2104@users.noreply.github.com>
2021-10-12 18:07:14 +0530

    Add files via upload

---
+++ newDelFile.py
@@ -0,0 +1,24 @@
+import os
+import time
+import json
+
+def getLocationAndTime(location):
+    store={}
+    for root , directories , files in os.walk(location):
+        for filename in files:
+            filepath=os.path.join(root,filename)
+            store[filepath]=os.stat(filepath).st_mtime
+    return sorted(store.items(), key=lambda x:x[1])
+
+def deleteFile(files,nFiles):
+    keep=len(files)-int(nFiles)
+    for i in range(0,keep):
+        os.remove(files[i][0])
+
+with open('D:\Python Project\location.json') as l:
+    loc=json.load(l)
+    if os.path.exists(loc['build']['location']):
+        files=getLocationAndTime(loc['build']['location'])
+        deleteFile(files,loc['build']['numberOfFiles'])
+    else:
+        print("Enter a valid file location")
